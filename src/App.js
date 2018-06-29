4b2d0b2dfe1fb6e8bc9d20cb94a7d3d28ca0ddbe
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-30 00:45:54 +0400

    [add] import data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './App.css';
 import FilterableProductTable from './components/filterable-product-table/filterable-product-table.js';
+import products from './products';
 
 class App extends React.Component {
   render() {
-    return <FilterableProductTable />;
+    return <FilterableProductTable products={products} />;
   }
 }
 export default App;
