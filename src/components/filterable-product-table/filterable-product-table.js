c5de70746611bf614489f1eab8c287480bccf913
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-30 00:44:23 +0400

    [add] filterable-product-table + styles

--- src/components/filterable-product-table/filterable-product-table.js
+++ src/components/filterable-product-table/filterable-product-table.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './style.css';
+import SearchBar from '../search-bar/search-bar';
+import ProductTable from '../product-table/product-table';
 
 export default class FilterableProductTable extends React.Component {
   render() {
-    return <h2 />;
+    return (
+      <div className="filterable-product-table">
+        <div className="filterable-product-table__wrapper">
+          <SearchBar />
+          <ProductTable products={this.props.products} />
+        </div>
+      </div>
+    );
   }
 }
