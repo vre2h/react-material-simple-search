042b1298de36777b59a69b1d3d1a3d871ee8e582
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-29 20:32:38 +0400

    init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+import Search from './search/search.js';
+
+class App extends React.Component {
+  render() {
+    return <Search />;
+  }
+}
+export default App;
---
+++ src/search/search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+class Search extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+export default Search;
