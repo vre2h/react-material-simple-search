88e08be2be26768befc233ad005a536d96a0d0c7
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-04 23:54:51 +0400

    [add] material ui styles

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import './style.css';
+import { TextField, Switch, FormControlLabel, Grid } from '@material-ui/core';
 
 export default class SearchBar extends React.Component {
   render() {
     return (
       <div className="search-bar">
-        <input
-          className="search-bar__input"
-          type="search"
-          value={this.props.value}
-          placeholder="Search..."
-          onChange={this.props.handleChangeOnInput}
-        />
-        <label className="search-bar-checkbox__title">
-          <input
-            className="search-bar-checkbox"
-            type="checkbox"
-            checked={this.props.checked}
-            name="checkbox"
-            id=""
-            onChange={this.props.handleChangeOnCheckBox}
+        <Grid container direction="column">
+          <TextField
+            className="search-bar__input"
+            type="search"
+            value={this.props.value}
+            placeholder="Search..."
+            onChange={this.props.handleChangeOnInput}
           />
-          {'Only show products in stock'}
-        </label>
+          <FormControlLabel
+            control={
+              <Switch
+                className="search-bar-checkbox"
+                type="checkbox"
+                checked={this.props.checked}
+                name="checkbox"
+                id=""
+                onChange={this.props.handleChangeOnCheckBox}
+                color="primary"
+              />
+            }
+            label={'Only show products in stock'}
+          />
+        </Grid>
       </div>
     );
   }
